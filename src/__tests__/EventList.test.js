49ac0eaa5e439fc931dfeee0164e280c477b2dd8
Terry Roe <terryroe@gmail.com>
2023-08-30 11:26:36 -0600

    move EventList integration scope out of component

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -22,16 +22,16 @@ describe('<EventList /> component', () => {
       allEvents.length
     );
   });
+});
 
-  describe('<EventList /> integration', () => {
-    test('renders a list of 32 events when the app is mounted and rendered', async () => {
-      const AppComponent = render(<App />);
-      const AppDOM = AppComponent.container.firstChild;
-      const EventListDOM = AppDOM.querySelector('#event-list');
-      await waitFor(() => {
-        const EventListItems = within(EventListDOM).queryAllByRole('listitem');
-        expect(EventListItems.length).toBe(32);
-      });
+describe('<EventList /> integration', () => {
+  test('renders a list of 32 events when the app is mounted and rendered', async () => {
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+    const EventListDOM = AppDOM.querySelector('#event-list');
+    await waitFor(() => {
+      const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+      expect(EventListItems.length).toBe(32);
     });
   });
 });

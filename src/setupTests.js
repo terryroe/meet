a55a3202137147a4fdfede2a12787f94f4172e62
Terry Roe <terryroe@gmail.com>
2023-10-18 13:02:29 -0600

    add charts to meet page

--- src/setupTests.js
+++ src/setupTests.js
@@ -21,3 +21,18 @@ console.error = (...args) => {
 };
 
 jest.setTimeout(30000);
+
+beforeEach(() => {
+  //@ts-ignore
+  delete window.ResizeObserver;
+  window.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  }));
+});
+
+afterEach(() => {
+  window.ResizeObserver = ResizeObserver;
+  jest.restoreAllMocks();
+});
